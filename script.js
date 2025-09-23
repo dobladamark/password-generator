90b01b43e17e11b3af966b9a9789f425616d350f
mark <122426238+dobladamark@users.noreply.github.com>
2025-09-23 12:47:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,12 @@ function generatePassword(length) {
   let result = "";
 
   for (let i = 0; i < length; i++) {
-    // Pick a random index
     const randomIndex = Math.floor(Math.random() * characters.length);
-    // Add that character to the result
     result += characters[randomIndex];
   }
 
   return result;
 }
-
-// Call the function with a desired length, e.g., 12
 let password = generatePassword(12);
 
-// Log result
 console.log("Generated password: " + password);
